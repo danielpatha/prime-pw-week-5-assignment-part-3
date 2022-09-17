3a435ed1c41fb023890e363130c898eb3e82591b
Daniel Pathammavong <patha037@umn.edu>
2022-09-17 13:35:43 -0500

    Finished addCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,22 @@
 console.log('***** Music Collection *****')
+
+let collection = [];
+
+function addToCollection(title, artist, yearPublished){
+let albumInfo = {title: title, artist: artist, yearPublished: yearPublished }
+collection.push(albumInfo);
+return albumInfo;
+}
+
+
+addToCollection('Victory Lap', 'Nipsey Hussle', '2018');
+addToCollection('Pink Planet', 'Pink $weats', '2021');
+addToCollection('Born Sinner', 'J.Cole', '2013');
+addToCollection('Ugh, Those Feels Again', 'Snoh Aalegra', '2019');
+addToCollection('The Man on the Moon: End of Day', 'Kid Cudi', '2009');
+addToCollection('Funhouse Mirror', 'Marlon Craft', '2019');
+console.log(collection);
+
+
+
+
